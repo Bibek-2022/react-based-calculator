483ef2c16819fb87175ca76d0ba21f91e2d5e8cd
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-03-20 14:02:01 +1100

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "./components/Button";
+import { Display } from "./components/Display";
+import { Title } from "./components/Title";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      {/* Title */}
+      <Title />
+      <div class="mainParent">
+        <Display />
+        <Button />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export const Button = () => {
+  return (
+    <div class="items">
+      <button class="clear">AC</button>
+      <button class="clean">C</button>
+      <button class="divide">/</button>
+      <button class="multi">*</button>
+      <button class="plus">+</button>
+      <button class="minus">-</button>
+      <button class="no-7">7</button>
+      <button class="no-8">8</button>
+      <button class="no-9">9</button>
+      <button class="no-4">4</button>
+      <button class="no-5">5</button>
+      <button class="no-6">6</button>
+      <button class="no-1">1</button>
+      <button class="no-2">2</button>
+      <button class="no-3">3</button>
+      <button class="no-0">0</button>
+      <button class="no-dot">.</button>
+      <button class="no-ans">=</button>
+    </div>
+  );
+};
