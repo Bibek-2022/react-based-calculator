fb4651aff512e9f1e4c10c43dc63a199956ef839
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-03-20 14:26:55 +1100

    Made the button Dynamic

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,87 @@
 import React from "react";
 
+const btns = [
+  {
+    level: "AC",
+    clsName: "clear",
+  },
+  {
+    level: "C",
+    clsName: "clean",
+  },
+  {
+    level: "/",
+    clsName: "divide",
+  },
+  {
+    level: "*",
+    clsName: "multi",
+  },
+  {
+    level: "+",
+    clsName: "plus",
+  },
+  {
+    level: "-",
+    clsName: "minus",
+  },
+  {
+    level: "7",
+    clsName: "no-7",
+  },
+  {
+    level: "8",
+    clsName: "no-8",
+  },
+  {
+    level: "9",
+    clsName: "no-9",
+  },
+  {
+    level: "4",
+    clsName: "no-4",
+  },
+  {
+    level: "5",
+    clsName: "no-5",
+  },
+  {
+    level: "6",
+    clsName: "no-6",
+  },
+  {
+    level: "1",
+    clsName: "no-1",
+  },
+  {
+    level: "2",
+    clsName: "no-2",
+  },
+  {
+    level: "3",
+    clsName: "no-3",
+  },
+  {
+    level: "0",
+    clsName: "no-0",
+  },
+  {
+    level: ".",
+    clsName: "no-dot",
+  },
+  {
+    level: "=",
+    clsName: "no-ans",
+  },
+];
 export const Button = () => {
   return (
     <div class="items">
-      <button class="clear">AC</button>
-      <button class="clean">C</button>
+      {btns.map((btn, i) => {
+        return <button className={btn.clsName}>{btn.level}</button>;
+      })}
+
+      {/* <button class="clean">C</button>
       <button class="divide">/</button>
       <button class="multi">*</button>
       <button class="plus">+</button>
@@ -20,7 +97,7 @@ export const Button = () => {
       <button class="no-3">3</button>
       <button class="no-0">0</button>
       <button class="no-dot">.</button>
-      <button class="no-ans">=</button>
+      <button class="no-ans">=</button> */}
     </div>
   );
 };
